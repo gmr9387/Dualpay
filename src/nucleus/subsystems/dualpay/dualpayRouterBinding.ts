8f96d7021726385fbdbad0778b80583e45f592f6
George M. Rios <gmr9387@gmail.com>
2026-08-31 10:37:03 -0400

    Update dualpayRouterBinding.ts

--- src/nucleus/subsystems/dualpay/dualpayRouterBinding.ts
+++ src/nucleus/subsystems/dualpay/dualpayRouterBinding.ts
@@ -1,7 +1,17 @@
 // src/nucleus/subsystems/dualpay/dualpayRouterBinding.ts
 
 import { dualpayRouter } from "./dualpayRouter";
+import { dualpayAdjudicationRouter } from "./dualpayAdjudicationRouter";
+import { dualpayCobRouter } from "./dualpayCobRouter";
+import { dualpayDenialRouter } from "./dualpayDenialRouter";
+import { dualpayRecoveryRouter } from "./dualpayRecoveryRouter";
+import { dualpayEvidenceRouter } from "./dualpayEvidenceRouter";
 
 export function bindDualpayRoutes(app: any) {
   app.use("/dualpay", dualpayRouter());
+  app.use("/dualpay", dualpayAdjudicationRouter());
+  app.use("/dualpay", dualpayCobRouter());
+  app.use("/dualpay", dualpayDenialRouter());
+  app.use("/dualpay", dualpayRecoveryRouter());
+  app.use("/dualpay", dualpayEvidenceRouter());
 }
