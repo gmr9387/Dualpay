d36a1f3a3d9e40554e70c4f4f4e175bc7f88e948
George M. Rios <gmr9387@gmail.com>
2026-08-31 08:46:29 -0400

    Delete src/nucleus/subsystems/dualpay directory

--- src/nucleus/subsystems/dualpay/dualpayRouterBinding.ts
+++
@@ -1,7 +0,0 @@
-// src/nucleus/subsystems/dualpay/dualpayRouterBinding.ts
-
-import { dualpayRouter } from "./dualpayRouter";
-
-export function bindDualpayRoutes(app: any) {
-  app.use("/dualpay", dualpayRouter());
-}
--- src/nucleus/subsystems/dualpay/index.ts
+++
@@ -1,9 +0,0 @@
-// src/nucleus/subsystems/dualpay/index.ts
-
-export * from "./dualpayRuntime";
-export * from "./dualpayController";
-export * from "./dualpayRouter";
-export * from "./dualpayRouterBinding";
-export * from "./dualpayOpenApi";
-export * from "./dualpayTelemetrySchema";
-export * from "./dualpayHealth";
